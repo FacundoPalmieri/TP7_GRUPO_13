fbbc3dce58de54ce79e8d6170b103033f5b1199f
krivera1807 <117855493+krivera1807@users.noreply.github.com>
2024-06-03 20:35:49 -0300

    Add files via upload

---
+++ AccesoDB.java
@@ -0,0 +1,55 @@
+package negocio;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+//Aca se programara la logica para conectarse a la DB
+//Comentar la contraseña incorrcta para la base de datos
+
+public class AccesoDB {
+
+	private static String host = "jdbc:mysql://localhost:3306/";
+	private static String user ="root";
+    private static String pass = "root";
+<<<<<<< HEAD
+    //private static String pass = "ROOT";
+=======
+//    private static String pass = "ROOT";
+>>>>>>> 68c844c888e74fc95e3802e2a5151f460511277a
+    private static String dbName = "segurosgroup";
+    private static Connection conexion;
+
+	
+	public static Connection getConnection() throws SQLException {
+        // Registrar el driver de MySQL
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("No se encontró el driver de MySQL", e);
+        }
+
+        // Obtener la conexión
+        try {
+        	conexion = DriverManager.getConnection(host + dbName , user , pass);
+        }
+        catch(Exception e){
+        	e.printStackTrace();
+        }
+        return conexion;
+    }
+
+    /**
+     * Cierra una conexión a la base de datos
+     * @param connection Conexión a cerrar
+     */
+    public static void closeConnection(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
